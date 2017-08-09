a365e0c2558218f48ccbf850467f565745f10f54
Chris Last <chris.last@gmail.com>
2017-08-09 10:43:01 +0100

    Correct follow mode steering and tweak some constants

--- cv_pibot.py
+++ cv_pibot.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 # import the necessary packages
-import numpy as np
+# import numpy as np
 import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
 import explorerhat as E
 import logging as LOG
 
@@ -14,20 +13,25 @@ SCAN = 1
 TRACK = 2
 FOLLOW = 3
 
-WIDTH=320
-HEIGHT=240
+WIDTH = 320
+HEIGHT = 240
+FRAMES_PER_SECOND = 30
+
+SCAN_TIMEOUT = 50   # ~2 seconds
+MAX_TARGET_SIZE = 80    # target radius
+MIN_TARGET_SIZE = 0.1   # target radius
 
 class Servo(object):
     """servo motors used by pan tilt device"""
     def __init__(self, pin, min, max):
         if pin in (E.OUT1, E.OUT2, E.OUT3, E.OUT4):
             self.servo = E.Output(pin)
-            self.min = min # -90
-            self.max = max # +90
-            self.frequency = 50 # Hz
+            self.min = min  # -90
+            self.max = max  # +90
+            self.frequency = 50  # Hz
             self.angle = 0
             self.point(self.angle)
-            LOG.log(99,"Controlling a Servo motor on pin %d",pin)
+            LOG.log(99, "Controlling a Servo motor on pin %d", pin)
         else:
             return None
     def point(self, degrees):
@@ -48,15 +52,15 @@ class Camera(object):
         """Create an object to capture video"""
         self.pan = Servo(E.OUT2, 88.61, 97.64)
         self.tilt = Servo(E.OUT1, 88.61, 96.39)
-        LOG.log(99,"Created a Pan/Tilt Camera Mount")
+        LOG.log(99, "Created a Pan/Tilt Camera Mount")
         self.picam = PiCamera()
-        LOG.log(99,"Camera started")
+        LOG.log(99, "Camera started")
         self.picam.resolution = (WIDTH, HEIGHT)
-        self.picam.framerate = 30
+        self.picam.framerate = FRAMES_PER_SECOND
         self.picam.rotation = 180
         # allow the camera to warmup
         time.sleep(0.1)
-        LOG.log(99,"Camera ready")
+        LOG.log(99, "Camera ready")
     def look(self, pan, tilt):
         """point the camera in a specific direction"""
         self.pan.point(pan)
@@ -71,7 +75,7 @@ class Driver(object):
         """Create an object to control vehicle motion"""
         self.left_wheel = E.Motor(E.M2B, E.M2F)
         self.right_wheel = E.Motor(E.M1B, E.M1F)
-        LOG.log(99,"Created a Robot Driver")
+        LOG.log(99, "Created a Robot Driver")
     def stop(self):
         """Stop both wheels"""
         self.left_wheel.stop()
@@ -89,15 +93,18 @@ class Driver(object):
 
 class Robot(object):
     def __init__(self):
-        """Create a robot object""" 
+        """Create a robot object"""
         self.camera = Camera()
-        self.camera.look(0,0)
+        self.camera.look(0, 0)
         self.driver = Driver()
         self.driver.stop()
         self.state = IDLE
         self.scan_timeout = 0
-        self.colours = { IDLE:E.light.blue, SCAN:E.light.red, TRACK:E.light.yellow, FOLLOW:E.light.green }
-        LOG.log(99,"Created a Robot")
+        self.colours = {IDLE: E.light.blue,
+                        SCAN: E.light.red,
+                        TRACK: E.light.yellow,
+                        FOLLOW: E.light.green}
+        LOG.log(99, "Created a Robot")
     def change(self, state):
         """Change or confirm the current robot state"""
         if self.state != state:
@@ -107,40 +114,37 @@ class Robot(object):
     def track(self, targetsize, x, y):
         """Track and/or chase an object detected by CV"""
         self.change(TRACK)
-        cpan = self.camera.pan.angle+(x-(WIDTH/2))/WIDTH*20
-        ctilt = robot.camera.tilt.angle+((HEIGHT/2)-y)/HEIGHT*20
+        cpan = self.camera.pan.angle + (x - (WIDTH / 2)) / WIDTH * 20
+        ctilt = robot.camera.tilt.angle + ((HEIGHT / 2) - y) / HEIGHT * 20
         self.camera.look(cpan, ctilt)
-        if ctilt > 20:
+        if ctilt > 15:
             self.driver.stop()
-        elif targetsize>20:
-            if abs(cpan) < 30:
-                self.driver.stop()
-            else:
-                self.driver.go(cpan,-cpan)
+        elif targetsize > MAX_TARGET_SIZE:
+                self.driver.go(cpan, -cpan)
         else:
-            if cpan>0:
-                self.driver.go(100-cpan,100)
+            if cpan > 0:
+                self.driver.go(100, 100 - cpan)
             else:
-                self.driver.go(100,100+cpan)
+                self.driver.go(100 + cpan, 100)
             self.change(FOLLOW)
         self.scan_timeout = 0
     def scan(self):
         """Look for an object by rotating camera and vehicle"""
-        if self.scan_timeout > 30:
+        if self.scan_timeout > SCAN_TIMEOUT:
             self.idle()
             return
         self.change(SCAN)
         if self.camera.tilt.angle > 0:
-            self.driver.go(100,-100)
+            self.driver.go(100, -100)
         else:
-            self.driver.go(-100,100)
+            self.driver.go(-100, 100)
         self.scan_timeout += 1
     def idle(self):
         """Stop looking but keep watch in front"""
         if self.state != IDLE:
             self.change(IDLE)
             self.driver.stop()
-            self.camera.look(0,0)
+            self.camera.look(0, 0)
             time.sleep(1)
             self.camera.sleep()
 
@@ -151,8 +155,8 @@ def cv_pibot(robot, display_windows=True):
     # define the lower and upper boundaries of the "green"
     # ball in the HSV color space, then initialize the
     # list of tracked points
-    greenLower = (0.11*256, 0.60*256, 0.20*256)
-    greenUpper = (0.14*256, 1.00*256, 1.00*256)
+    greenLower = (0.11 * 256, 0.60 * 256, 0.20 * 256)
+    greenUpper = (0.14 * 256, 1.00 * 256, 1.00 * 256)
     # initialise target lost counter
     lost_count = 0
     # capture frames from the camera
@@ -192,19 +196,19 @@ def cv_pibot(robot, display_windows=True):
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             # only proceed if the radius meets a minimum size
-            if radius > 0.1:
+            if radius > MIN_TARGET_SIZE:
                 lost_count = 0
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
                 cv2.circle(hsv, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                 cv2.circle(hsv, center, 5, (0, 0, 255), -1)
-                cv2.putText(hsv, "x=%d, y=%d" % (x,y), (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
-                cv2.putText(hsv, str(image[y,x].tolist()) + " - " + str(radius), (10,230), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+                cv2.putText(hsv, "x=%d, y=%d" % (x, y), (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+                cv2.putText(hsv, str(image[y, x].tolist()) + " - " + str(radius), (10,230), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
                 # Put Robot into target tracking mode
-                robot.track(radius,x,y)
+                robot.track(radius, x, y)
         else:
             # Put Robot into target scanning mode
-            if lost_count > 3:
+            if lost_count > 4:
                robot.scan()
             else:
                 lost_count += 1
